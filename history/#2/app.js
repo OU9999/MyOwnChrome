a7d17966761558fee0762814e3bfb9ac52d13d25
omh232323 <omh232323@gmail.com>
2022-11-16 23:30:09 +0900

    132

--- history/#2/app.js
+++ history/#2/app.js
@@ -2,6 +2,6 @@ const age = parseInt(prompt("How old are you?"));
 
 if (isNaN(age)) {
   console.log("it's not num");
-} else {
-  console.log("it's num");
+} else if (age < 18) {
+  console.log("you're too young");
 }
