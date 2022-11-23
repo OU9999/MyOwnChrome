9e1a41c5ce68d78876b15669059725ac39c7b866
오유진 <omh232323@gmail.com>
2022-11-23 20:56:10 +0900

    1123

--- history/#2/app.js
+++ history/#2/app.js
@@ -1,7 +1,16 @@
-const age = parseInt(prompt("How old are you?"));
-
-if (isNaN(age)) {
-  console.log("it's not num");
-} else if (age < 18) {
-  console.log("you're too young");
+const title = document.querySelector(".hello .title");
+/**안녕 */
+function clickEvent() {
+  title.innerText = "You Clicked!";
 }
+
+function mouseEnterEvent() {
+  title.style.color = "red";
+}
+function mouseLeaveEvent() {
+  title.style.color = "blue";
+}
+
+title.addEventListener("click", clickEvent);
+title.addEventListener("mouseenter", mouseEnterEvent);
+title.addEventListener("mouseleave", mouseLeaveEvent);
