582c5c8f0e185e1eaa2ab13bf408eb7a2d7444c4
오유진 <omh232323@gmail.com>
2022-11-26 16:28:51 +0900

    565656

--- history/#2/app.js
+++ history/#2/app.js
@@ -1,16 +1,7 @@
-const title = document.querySelector(".hello .title");
+const title = document.querySelector(".title");
 /**안녕 */
 function clickEvent() {
-  title.innerText = "You Clicked!";
-}
-
-function mouseEnterEvent() {
-  title.style.color = "red";
-}
-function mouseLeaveEvent() {
-  title.style.color = "blue";
+  title.classList.toggle("title__active");
 }
 
 title.addEventListener("click", clickEvent);
-title.addEventListener("mouseenter", mouseEnterEvent);
-title.addEventListener("mouseleave", mouseLeaveEvent);
